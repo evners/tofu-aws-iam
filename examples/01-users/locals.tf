8aac2de230eb0d68578c36f23fdbfcbe83012326
Guillem Casas <gucasassi@users.noreply.github.com>
2025-11-15 08:47:57 +0100

    docs(users): add example to create user with console access and access keys

--- examples/01-users/locals.tf
+++ examples/01-users/locals.tf
@@ -4,5 +4,9 @@ locals {
   iam_users = [
     # Define a simple user.
     { name = "john.doe" },
+    # Define a user with console access.
+    { name = "jane.smith", console_access = true },
+    # Define a user with access key.
+    { name = "org.github", access_key = true }
   ]
 }
